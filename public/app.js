78decf0e77ee58c2f3eec9893a6df07d1cd282a1
Bharatkhatri <bharatkhatri351@gmail.com>
2021-02-28 15:12:40 +0530

    css adjusted of devpage button and heading and added preloader

--- public/app.js
+++ public/app.js
@@ -2,3 +2,8 @@ function actionToggle(){
     var action = document.querySelector('.action');
     action.classList.toggle('active')
 }
+ //after window is loaded completely 
+ window.onload = function(){
+    //hide the preloader
+    document.querySelector(".preloader").style.display = "none";
+}
