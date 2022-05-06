043a31d13cb5a6ca6068994ff12c1165c6ae8219
Blacklion14 <rkush214@gmail.com>
2022-05-06 19:01:19 +0530

    Added Readme File and some validations

--- mongoose/db.js
+++ mongoose/db.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect(process.env.MONGO_URL || "mongodb+srv://webstartup:Test123456@cluster0.jlc5g.mongodb.net/knit", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, (err) => {
-  if (!err) {
-    console.log("u are connected with mongoose");
-  } else {
-    console.log("u r failed " + err);
-  }
-});
+try{
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (err) => {
+    if (!err) {
+      console.log("u are connected with mongoose");
+    } else {
+      console.log("u r failed " + err);
+    }
+  });
+} catch {
+  console.log("Setup Mongoose");
+}
 
 
 
